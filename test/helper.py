ddbf7159de1b3051bd42c0977344d6111d18b9f7
vaishakp <vaishakprasad@gmail.com>
2023-04-24 12:21:16 +0530

    remove : in msg

--- test/helper.py
+++ test/helper.py
@@ -5,60 +5,72 @@ import config
 import datetime
 from inspect import getframeinfo, getmodule, stack
 import os
+
+
 def message(
-    *args, message_verbosity=2, print_verbosity=config.print_verbosity, log_verbosity=config.log_verbosity, **kwargs):
-    ''' The print function with verbosity levels and logging facility.
+    *args, message_verbosity=2, print_verbosity=config.print_verbosity, log_verbosity=config.log_verbosity, **kwargs
+):
+    """The print function with verbosity levels and logging facility.
+
     Notes
     -----
+
     Verbosity choices:
+
                     message_verbosity   :   Each message carries with it a verbosity level. More the verbosity more the priority. Default value is 2
+
                     print_verbosity     :   prints all messages above this level of verbosity.
+
                     log_verbosity       :   logs all messages above this level of verbosity.
+
     Verbosity  levels:
+
                     0: Errors
+
                     1: Warnings
+
                     2: Information
+
     Parameters
     ----------
+
                     ``*args`            :   non-keyword arguments
+                                                                                                                    same arguments as to that of the print functions,
+
                     message_verbosity   :   int
+
                     print_verbosity     :   int
+
                     log_verbosity       :   int
+
                     ``**kwargs``        :   keyword arguments
-                                            Same as that of the print function.
+                                                                                                                    Same as that of the print function.
+
     Returns
     -------
-                    1                   :   int                                                                     
-                                            messages to stdout and logging of messages, while the function returns 1.'''
+
+                    1                   :   int
+                                                                                                                    messages to stdout and logging of messages, while the function returns 1."""
 
     # If message verbosity matches the global verbosity level, then print
     if message_verbosity <= print_verbosity:
         print(*args, **kwargs)
     if log_verbosity <= message_verbosity:
         now = str(datetime.datetime.now())
-        tstamp = now[:10] + "_" + now[11:16]
+        tstamp = (now[:10] + "_" + now[11:16]).replace(':', '-')
         caller = getframeinfo(stack()[1][0])
         # frameinfo = getframeinfo(currentframe())
         if not os.path.isdir("logs"):
             os.mkdir("logs")
-    
-        #frame = stack()[1]
-        #module = getmodule(frame[0])
-        #fname = module.__file__
-        fname = caller
-        #fname = os.path.basename(__file__)
-        with open(f"logs/" + tstamp + ".log", "a") as log_file:
+
+        with open("logs/" + tstamp + ".log", "a") as log_file:
             if message_verbosity == -1:
-                log_file.write('\n')
-                log_file.write(caller)
                 for line in traceback.format_stack():
                     log_file.write(line.strip())
             log_file.write("\n")
             log_file.write("{}:{}\t{}".format(caller.filename, caller.lineno, *args))
             log_file.write("\n")
     return 1
-
-
 
 def TransformSpinsNRtoLAL(nrSpin1, nrSpin2, n_hat, ln_hat):
     ''' Trnasform the spins of the NR simulation from the
